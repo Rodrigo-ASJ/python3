e63a6f054dacd456e0b1386843bae685ec5ad680
Rodrigo <rodrigo@asmalljob.com>
2026-09-24 16:13:17 +0200

    update: src/

---
+++ src/for.py
@@ -0,0 +1,6 @@
+for i in range(3):
+  print(i)
+i = 1
+while i < 6:
+  print(f"ahora i vale:  {i}")
+  i += 1
---
+++ src/greetings.py
@@ -0,0 +1,10 @@
+def main():
+  print('Hellow World from python')
+
+def second():
+  print(__name__)
+
+second()
+
+if __name__ == '__main__':
+  main()
